45bb9a2004e03146920b37dba77019d79c447fdd
ArpanSaha404 <160222105+ArpanSaha404@users.noreply.github.com>
2024-10-08 17:08:25 +0530

    Update MainPage.jsx

--- src/Components/mainPage/MainPage.jsx
+++ src/Components/mainPage/MainPage.jsx
@@ -20,7 +20,7 @@ const MainPage = () => {
     useEffect(() => {
         const fetchDetails = async () => {
             await axios
-                .get(`https://travello-r7hg.onrender.com`)
+                .get(`https://travello-r7hg.onrender.com/topPackages`)
                 .then((res) => {
                     console.log(res.data);
                     if(res.data && res.data.topPackages.length > 0)
