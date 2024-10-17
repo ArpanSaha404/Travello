d5ee517680dc557ed2c214a15204b5bda7eac022
ArpanSaha404 <arpan50saha@gmail.com>
2024-10-17 18:39:12 +0530

    Fixed Booking Page

--- backend/routes/bookPackagesRouter.js
+++ backend/routes/bookPackagesRouter.js
@@ -5,10 +5,108 @@ import { BookedPackages } from '../models/bookedPackages.js';
 
 const router = express.Router();
 
+router.post('/insertBookPackageData/:packageID' , async (req , res) => {
+    try {
+        const pkgId = req.params.packageID.toLowerCase();
+        const {userEmail} = req.body;
+        console.log(`Package : ${pkgId} & Email : ${userEmail}`);
+
+        const user = await Users
+            .findOne({email : userEmail});
+
+        const bookingPackageData = await PackageDetails
+            .findOne({packageTitle : pkgId});
+        
+        if(user && bookingPackageData)
+        {
+            let newBookingID , newPaymentID;
+            for(let i=0;i<9999;i++)
+            {
+                newBookingID = 'BID' + Math.floor(10000 + Math.random() * 90000);
+                const dupBID = await BookedPackages
+                    .findOne({bookingID : newBookingID})
+                if(!dupBID)
+                {
+                    console.log(newBookingID);
+                    break;
+                }     
+            }
+            for(let i=0;i<9999;i++)
+            {
+                newPaymentID = 'PID' + Math.floor(10000 + Math.random() * 90000);
+                const dupPID = await BookedPackages
+                    .findOne({paymentID : newPaymentID})
+                if(!dupPID)
+                {
+                    console.log(newPaymentID);
+                    break;
+                }     
+            }
+            const newBookedPackage = new BookedPackages({
+                packageName : pkgId,
+                bookedBy : userEmail,
+                bookingId : newBookingID,
+                paymentID : newPaymentID,
+                bookingStatus : 'saved',
+                bookedHotels : 'false',
+            });
+            const insertedData = await BookedPackages.create(newBookedPackage);
+            if(insertedData)
+            {
+                user.bookedPackages.push(newBookedPackage)
+                user.save();
+                console.log(`Package Saved with Booing ID : ${newBookingID}`);
+                res.status(200).json({insertedData});
+            }
+        }
+        else
+        {
+            if(!user)
+            {
+                res.status(200).json({message : "No Such User Found"});
+            }
+            else
+            {
+                res.status(200).json({message : "No Such Package Found"});
+            }
+        }
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({message : 'Some Error in Inserting Booked Package Data'});
+    }
+});
+
+router.get('/bookingDetails/:packageId/:bookingId' , async (req , res) => {
+    const pkgId = req.params.packageId.toLowerCase();
+    const bId = 'BID' + req.params.bookingId.substring(3);
+
+    try {
+        const insertedData = await BookedPackages
+            .findOne({bookingId : bId} , {_id : 0});
+
+        const bookingPackageData = await PackageDetails
+            .findOne({packageTitle : pkgId} , {_id : 0})
+    
+        if(insertedData && bookingPackageData)
+        {
+            res.status(200).json({bookingPackageData , insertedData})
+        }
+        else
+        {
+            res.status(200).json({message : 'No Such Booking Found!!!'});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({message : 'Some Error!!!'});
+    }
+})
+
 router.get('/bookPackage/:packageId' , async (req , res) => {
     try {
         const pkgId = req.params.packageId.toLowerCase();
-        const email = req.query.email;
+        const {email} = req.query;
         console.log(email);
 
         const user = await Users
